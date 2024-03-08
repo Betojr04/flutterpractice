5c9936b58e26dda4c7b178122ae483090c3175fb
Betojr04 <betomasia12@gmail.com>
2024-03-08 15:32:47 -0700

    added a bit more content to the hero section

--- client/src/components/LandingPageComponents/HeroSection.js
+++ client/src/components/LandingPageComponents/HeroSection.js
@@ -6,6 +6,12 @@ export const HeroSection = () => {
     <section className="hero-container">
       <div className="hero-text">
         <h2>Easy Way To Finance</h2>
+        <p>
+          Take Control Of Your Finances and Steer Them in The Right Direction
+        </p>
+      </div>
+      <div>
+        <img src="#" />
       </div>
     </section>
   );
